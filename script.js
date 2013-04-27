87b8aab9785fa8ad7ca567631891a44d5f21eec6
Eugene <fncombo@gmail.com>
2013-04-27 08:03:43 +0100

    Added support for infinite scrolling.

--- script.js
+++ script.js
@@ -4,95 +4,126 @@
  * @version 0.2
 */
 
-// Get each image post
-Array.prototype.slice.call(document.querySelectorAll('.post.photo img')).forEach(function (el) {
+(function () {
 
     'use strict';
 
-        // Create the download button
-    var save = document.createElement('div'),
-        // If the image is inside a link, then it means we can get a HD version
-        hd = el.parentNode.classList.contains('high_res_link');
+    // Add download buttons to image posts
+    function addButtons() {
 
-    save.innerHTML = 'Download' + (hd ? ' <strong>HD</strong>' : '');
-    save.classList.add('__download');
+        // Get each image post
+        Array.prototype.slice.call(document.querySelectorAll('.post.photo img:not(.__ignore)')).forEach(function (el) {
 
-    // Download the image on click
-    save.onclick = function () {
+            // Add a class to the image so we don't add a button to it again
+            el.classList.add('__ignore');
 
-            // Create the download link
-        var link = document.createElement('a'),
-            // Create the click event
-            event = document.createEvent('Event');
+                // Create the download button
+            var save = document.createElement('div'),
+                // If the image is inside a link, then it means we can get a HD version
+                hd = el.parentNode.classList.contains('high_res_link');
 
-        // If a HD version is available, then replace the download URL with the one for high resolution image
-        link.href = hd ? el.src.replace(/\d+(?=\.(jpe?g|png|gif)$)/g, '1280') : el.src;
+            save.innerHTML = 'Download' + (hd ? ' <strong>HD</strong>' : '');
+            save.classList.add('__download');
 
-        link.target = '_blank';
+            // Download the image on click
+            save.onclick = function () {
 
-        // Extract the image file name from the link
-        link.download = link.href.match(/tumblr_\w+\.(jpe?g|png|gif)$/g);
+                    // Create the download link
+                var link = document.createElement('a'),
+                    // Create the click event
+                    event = document.createEvent('Event');
 
-        // Configure the click event
-        event.initEvent('click', true, true);
+                // If a HD version is available, then replace the download URL with the one for high resolution image
+                link.href = hd ? el.src.replace(/\d+(?=\.(jpe?g|png|gif)$)/g, '1280') : el.src;
 
-        // Trigger the click event on the link
-        link.dispatchEvent(event);
+                link.target = '_blank';
 
-        // Free some memory or something
-        window.URL.revokeObjectURL(link.href);
+                // Extract the image file name from the link
+                link.download = link.href.match(/tumblr_\w+\.(jpe?g|png|gif)$/g);
 
-    };
+                // Configure the click event
+                event.initEvent('click', true, true);
 
-    // Highlight the image when hovering over download link
-    save.onmouseover = function () {
+                // Trigger the click event on the link
+                link.dispatchEvent(event);
 
-        if (hd) {
-            this.parentNode.classList.add('__highlight');
-        } else {
+                // Free some memory or something
+                window.URL.revokeObjectURL(link.href);
 
-            this.previousSibling.classList.add('__highlight');
+            };
 
-            // If the image is in a photoset row, reveal the row to show the whole image
-            if (this.parentNode.parentNode.classList.contains('photoset_row')) {
-                this.parentNode.parentNode.classList.add('__show_photoset');
+            // Highlight the image when hovering over download link
+            save.onmouseover = function () {
+
+                if (hd) {
+                    this.parentNode.classList.add('__highlight');
+                } else {
+
+                    this.previousSibling.classList.add('__highlight');
+
+                    // If the image is in a photoset row, reveal the row to show the whole image
+                    if (this.parentNode.parentNode.classList.contains('photoset_row')) {
+                        this.parentNode.parentNode.classList.add('__show_photoset');
+                    }
+
+                }
+
+            };
+
+            // Remove the highlight when not hovering
+            save.onmouseout = function () {
+
+                if (hd) {
+                    this.parentNode.classList.remove('__highlight');
+                } else {
+
+                    this.previousSibling.classList.remove('__highlight');
+
+                    if (this.parentNode.parentNode.classList.contains('photoset_row')) {
+                        this.parentNode.parentNode.classList.remove('__show_photoset');
+                    }
+
+                }
+
+            };
+
+            // Add a special class to the parent of the download button
+            // Insert the download button
+            if (hd) {
+
+                var parent = el.parentNode.parentNode;
+                parent.classList.add('__download_parent');
+                parent.insertBefore(save, parent.lastChild);
+
+            } else {
+
+                el.parentNode.classList.add('__download_parent');
+                el.parentNode.insertBefore(save, el.nextSibling);
+
             }
 
-        }
-
-    };
-
-    // Remove the highlight when not hovering
-    save.onmouseout = function () {
-
-        if (hd) {
-            this.parentNode.classList.remove('__highlight');
-        } else {
-
-            this.previousSibling.classList.remove('__highlight');
-
-            if (this.parentNode.parentNode.classList.contains('photoset_row')) {
-                this.parentNode.parentNode.classList.remove('__show_photoset');
-            }
-
-        }
-
-    };
-
-    // Add a special class to the parent of the download button
-    // Insert the download button
-    if (hd) {
-
-        var parent = el.parentNode.parentNode;
-        parent.classList.add('__download_parent');
-        parent.insertBefore(save, parent.lastChild);
-
-    } else {
-
-        el.parentNode.classList.add('__download_parent');
-        el.parentNode.insertBefore(save, el.nextSibling);
+        });
 
     }
 
+    // Add buttons to initially loaded images
+    addButtons();
 
-});
+    // If #pagination is hidden, check if new posts have been added with each scroll
+    if (document.getElementById('pagination').classList.contains('hidden')) {
+
+        var previousHeight = document.height;
+
+        document.onscroll = function () {
+
+            // If the document increased in height while scrolling, assume new posts have been loaded
+            if (previousHeight < document.height) {
+                previousHeight = document.height;
+                addButtons();
+            }
+
+        };
+
+    }
+
+}());
