4886eff15c5514d48798bb5799f1029d9da27ae1
Eugene <fncombo@gmail.com>
2013-04-27 09:23:00 +0100

    Recognise more HD images.

--- script.js
+++ script.js
@@ -20,13 +20,35 @@
                 // Create the download button
             var save = document.createElement('div'),
                 // If the image is inside a link, then it means we can get a HD version
-                hd = el.parentNode.classList.contains('high_res_link');
+                hd = el.parentNode.classList.contains('high_res_link'),
+                // Regular expression to match the size of the image
+                matchSize = new RegExp('\\d+(?=\\.(jpe?g|png|gif)$)', 'g'),
+                // Another type of HD image link
+                softHd = el.parentNode.nodeName === 'A' && el.parentNode.href.match(matchSize) !== null ? true : false,
+                // See if the soft HD image is bigger than the one displayed on the page
+                softHdSize = softHd ? parseInt(el.src.match(matchSize)[0], 10) < parseInt(el.parentNode.href.match(matchSize)[0], 10) : false;
 
-            save.innerHTML = 'Download' + (hd ? ' <strong>HD</strong>' : '');
+            save.innerHTML = 'Download' + (hd || softHdSize ? ' <strong>HD</strong>' : '');
             save.classList.add('__download');
 
             // Download the image on click
             save.onclick = function () {
+
+                if (el.parentNode.nodeName === 'A') {
+
+                    var originalEvent = el.parentNode.onclick;
+
+                    // Suppress the click on the link
+                    el.parentNode.onclick = function () {
+                        return false;
+                    };
+
+                    // Restore the click event
+                    setTimeout(function () {
+                        el.parentNode.onclick = originalEvent;
+                    }, 100);
+
+                }
 
                     // Create the download link
                 var link = document.createElement('a'),
@@ -34,9 +56,10 @@
                     event = document.createEvent('Event');
 
                 // If a HD version is available, then replace the download URL with the one for high resolution image
-                link.href = hd ? el.src.replace(/\d+(?=\.(jpe?g|png|gif)$)/g, '1280') : el.src;
+                link.href = hd ? el.src.replace(matchSize, '1280') : el.src;
 
-                link.target = '_blank';
+                // If another type of HD version is available, prefer that
+                link.href = softHd ? el.parentNode.href : link.href;
 
                 // Extract the image file name from the link
                 link.download = link.href.match(/tumblr_\w+\.(jpe?g|png|gif)$/g);
@@ -91,6 +114,7 @@
             // Insert the download button
             if (hd) {
 
+                // If HD image, go up another tag to avoid inserting the button into an anchor tag
                 var parent = el.parentNode.parentNode;
                 parent.classList.add('__download_parent');
                 parent.insertBefore(save, parent.lastChild);
