ab36652216c5f78508bd2c9928264e0b0732e320
Eugene <fncombo@gmail.com>
2014-06-14 11:06:52 +0100

    Refined analytic options

--- js/pages/background.js
+++ js/pages/background.js
@@ -7,12 +7,21 @@ window._gaq = window._gaq || [];
 window._gaq.push(['_setAccount', 'UA-40682860-1']);
 window._gaq.push(['_trackPageview']);
 
-var ga = document.createElement('script');
-ga.type = 'text/javascript';
-ga.async = true;
-ga.src = 'https://ssl.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(ga, s);
+// Only add analytics if they haven't disabled it
+chrome.storage.sync.get({enableAnalytics: true}, function (object) {
+
+    if (!object.enableAnalytics) {
+
+        var ga = document.createElement('script');
+        ga.type = 'text/javascript';
+        ga.async = true;
+        ga.src = 'https://ssl.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(ga, s);
+
+    }
+
+});
 
 // When the extension is first installed, updated, or when Chrome is updated
 chrome.runtime.onInstalled.addListener(function (details) {
