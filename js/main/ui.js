affd87289e80e5e352ee666f4a405438e56f6bec
Eugene <fncombo@gmail.com>
2015-04-13 14:19:17 +0100

    Improved dialog
    can now have multiple dialogue opens at that same time with unique
    events
    can pass an optional image url to have it display as a thumbnail

--- js/main/ui.js
+++ js/main/ui.js
@@ -13,60 +13,70 @@ TID.ui = {};
  * @param {String}   message  Message of the dialog
  * @param {Array}    options  Array of buttons
  * @param {Function} callback The callback to call when a button is clicked
+ * @param {String}   imageUrl Optional URL of an image to display a thumbnail of
  */
-TID.ui.showDialog = function (message, options, callback) {
+TID.ui.showDialog = function (message, options, callback, imageUrl) {
     if (!(options instanceof Array)) {
         options = [options];
     }
 
-    var html = '';
-
-    html += '<div class="' + TID.classes.overlay + '">';
-    html += '<div class="' + TID.classes.dialog + '">';
-    html += '<div class="' + TID.classes.dialogMessage + '">' + message + '</div>';
-
-    options.forEach(function (option, i) {
-        html += '<div class="' + TID.classes.dialogButton + '" data-i="' + i + '">';
-        html += option;
-        html += '</div>';
-    });
-
-    html += '</div>';
-    html += '</div>';
-
     // Create a new element using DOM functions and append to that
     // as appending straight to the body unbinds events...
-    var tempContainer = $('.' + TID.classes.tempContainer);
-
-    if (tempContainer) {
-        tempContainer.remove();
-    }
-
-    tempContainer = document.createElement('div');
+    var tempContainer = document.createElement('div');
     tempContainer.classList.add(TID.classes.tempContainer);
-    tempContainer.innerHTML = html;
-    document.body.appendChild(tempContainer);
 
-    function listener(event) {
-        callback.call(event, event.target.dataset.i);
+    var container = document.createElement('div');
+    container.classList.add(TID.classes.tempContainer);
+    tempContainer.appendChild(container);
 
-        $$('.' + TID.classes.dialogButton).forEach(function (el) {
-            el.removeEventListener('click', listener, true);
-        });
+    var overlay = document.createElement('div');
+    overlay.classList.add(TID.classes.overlay);
+    container.appendChild(overlay);
 
-        $('.' + TID.classes.overlay).remove();
+    var dialog = document.createElement('div');
+    dialog.classList.add(TID.classes.dialog);
+    overlay.appendChild(dialog);
+
+    var dialogMessage = document.createElement('div');
+    dialogMessage.classList.add(TID.classes.dialogMessage);
+
+    if (imageUrl) {
+        var dialogImage = document.createElement('div');
+        dialogImage.classList.add(TID.classes.dialogImage);
+        dialogImage.innerHTML = '<img src="' + imageUrl + '">';
+        dialogMessage.appendChild(dialogImage);
     }
 
-    $$('.' + TID.classes.dialogButton).forEach(function (el) {
-        el.addEventListener('click', listener, true);
+    dialogMessage.innerHTML += message;
+    dialog.appendChild(dialogMessage);
+
+    options.forEach(function (option, i) {
+        var button = document.createElement('div');
+        button.classList.add(TID.classes.dialogButton);
+        button.dataset.i = i;
+        button.innerHTML = option;
+
+        button.onclick = function () {
+            if (callback) {
+                // i must be a string
+                callback.call(event, button.dataset.i);
+            }
+
+            tempContainer.remove();
+        };
+
+        dialog.appendChild(button);
     });
+
+    document.body.appendChild(tempContainer);
 };
 
 /**
  * Show a download confirmation dialog
  * @param {Function} callback The callback to call once the user answers
+ * @param {String}   imageUrl Optional URL of an image to display a thumbnail of
  */
-TID.ui.confirmDialog = function (callback) {
+TID.ui.confirmDialog = function (callback, imageId) {
     var message = TID.msg('confirmDuplicateDownload');
     var buttons = [TID.msg('yes'), TID.msg('no')];
 
@@ -74,5 +84,5 @@ TID.ui.confirmDialog = function (callback) {
         callback(i === '0' ? true : false);
     }
 
-    TID.ui.showDialog(message, buttons, buttonsCallback);
+    TID.ui.showDialog(message, buttons, buttonsCallback, imageId);
 };
